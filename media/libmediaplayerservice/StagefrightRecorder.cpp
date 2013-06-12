042a0608e0c5d675f37b4c9f4aa855512c0fd240
cphelps76 <cphelps76@gmail.com>
2013-06-12 11:09:07 -0500

    Revert "libstagefright: support for disabling buffer metadata"
    This reverts commit 15e47987066fb32d84bcae830d8b8760b1f15c6c.

--- media/libmediaplayerservice/StagefrightRecorder.cpp
+++ media/libmediaplayerservice/StagefrightRecorder.cpp
@@ -1460,17 +1460,9 @@ status_t StagefrightRecorder::setupCameraSource(
                 mTimeBetweenTimeLapseFrameCaptureUs);
         *cameraSource = mCameraSourceTimeLapse;
     } else {
-        bool useMeta = true;
-#ifdef QCOM_HARDWARE
-        char value[PROPERTY_VALUE_MAX];
-        if (property_get("debug.camcorder.disablemeta", value, NULL) &&
-            atoi(value)) {
-            useMeta = false;
-        }
-#endif
         *cameraSource = CameraSource::CreateFromCamera(
                 mCamera, mCameraProxy, mCameraId, videoSize, mFrameRate,
-                mPreviewSurface, useMeta /*storeMetaDataInVideoBuffers*/);
+                mPreviewSurface, true /*storeMetaDataInVideoBuffers*/);
     }
     mCamera.clear();
     mCameraProxy.clear();
